3f7999744f6e0dc99e39a69b36a4aebcc4906206
Jose Julio Figueiredo Junior <josejuliojunior@hotmail.com>
2017-07-26 14:50:48 -0600

    cors

--- routes/wached_movies.js
+++ routes/wached_movies.js
@@ -1,6 +1,10 @@
 var express = require("express");
 var router = express.Router();
 var queries = require("../queries");
+var cors = require("cors");
+
+app.use(cors());
+
 
 router.get("/", function(request, response, next){
   queries.getMovies().then(function(movies){
