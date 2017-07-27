26e262f394f3a2d534aada6eb44ef53c773b18c1
Jose Julio Figueiredo Junior <josejuliojunior@hotmail.com>
2017-07-27 15:09:20 -0600

    id

--- queries.js
+++ queries.js
@@ -3,7 +3,6 @@ const knex = require('./connection');
 module.exports = {
   getMovies: function(){
   return knex('wached_movies')
-  .select('wached_movies.name','wached_movies.rate','wached_movies.description','wached_movies.type','wached_movies.duration');
   },
   getOneMovie: function(id){
     return knex('wached_movies')
